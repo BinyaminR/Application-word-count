b418ce186917c10b160cdc9f6b7f906180aa9743
BinyaminR <binyamin.rothenberg@gmail.com>
2023-08-22 21:12:32 +0300

    v1.0.1

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import sys
 app = Flask(__name__)
 
 
+
 #client = MongoClient("mongodb://mongodb:27017/")  # Use the correct connection string format
 db_url = os.environ.get("DB_URL")
 user_name = os.environ.get("USER_NAME")
