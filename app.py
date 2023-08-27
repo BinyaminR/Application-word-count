5cacac01b40df2225d08a2b2bc3e3245e227f43a
BinyaminR <binyamin.rothenberg@gmail.com>
2023-08-27 20:25:21 +0300

    1.0.4

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Words Counter and Paragraphs Counter Flask App using Python
 
-# Words Counter and Paragraphs Counter Flask 
+# Words Counter and Paragraphs Counter 
 
 from flask import Flask, request, render_template
 from datetime import datetime
